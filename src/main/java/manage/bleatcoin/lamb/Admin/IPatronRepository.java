dee0b3b1060f512d49dcbd67f467b7f5d41c889a
KirkWorl <kworley@csumb.edu>
2019-06-08 22:21:59 -0700

    Changes to extends part of IPatronRepository to deal with int-string conversion problems.

--- src/main/java/manage/bleatcoin/lamb/Admin/IPatronRepository.java
+++ src/main/java/manage/bleatcoin/lamb/Admin/IPatronRepository.java
@@ -8,4 +8,4 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface IPatronRepository extends MongoRepository<Patron, String> { }
+public interface IPatronRepository extends MongoRepository<Patron, int> { }
