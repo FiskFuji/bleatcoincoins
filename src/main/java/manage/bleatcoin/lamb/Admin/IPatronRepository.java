6b1dce8e78bf9842ac62b645eb09e7d894186431
KirkWorl <kworley@csumb.edu>
2019-06-08 22:04:18 -0700

    I misspelled entities

--- src/main/java/manage/bleatcoin/lamb/Admin/IPatronRepository.java
+++ src/main/java/manage/bleatcoin/lamb/Admin/IPatronRepository.java
@@ -1,6 +1,7 @@
 package manage.bleatcoin.lamb.Admin;
 
 import manage.bleatcoin.lamb.Admin.entities.Patron;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
