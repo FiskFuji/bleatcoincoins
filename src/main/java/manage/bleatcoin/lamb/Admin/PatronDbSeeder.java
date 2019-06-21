4b13a35e20e57a570742b2a9d5847c2dcad3ab2d
KirkWorl <kworley@csumb.edu>
2019-06-21 14:47:26 -0700

    Removing seeding

--- src/main/java/manage/bleatcoin/lamb/Admin/PatronDbSeeder.java
+++ src/main/java/manage/bleatcoin/lamb/Admin/PatronDbSeeder.java
@@ -1,13 +1,9 @@
 package manage.bleatcoin.lamb.Admin;
 
-import manage.bleatcoin.lamb.Admin.entities.Patron;
 import manage.bleatcoin.lamb.Admin.PatronService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Component
 public class PatronDbSeeder implements CommandLineRunner {
@@ -17,15 +13,6 @@ public class PatronDbSeeder implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Patron testP = new Patron();
-        testP.setId(0);
-        testP.setCoins(10);
-        testP.setUsername("Tycho");
-        testP.setRealname("Fisk");
-        testP.setTier("Mantid");
-
-        serv.deleteAllPatrons();
-        List<Patron> patrons = Arrays.asList(testP);
-        serv.createPatron(patrons);
+        // --- DB Seeding complete. Do nothing ---
     }
 }
